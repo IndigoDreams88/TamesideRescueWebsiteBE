d15271450cdc194df55bbc3b44ee696f6c1d01ac
roberthaworth1234 <roberthaworth1234@hotmail.com>
2020-06-11 15:54:10 +0100

    updated controller

--- src/main/java/com/guineapigsanctuary/demo/api/AnimalController.java
+++ src/main/java/com/guineapigsanctuary/demo/api/AnimalController.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
-@RequestMapping("/")
+@RequestMapping("api/v1/animal")
 @RestController
 public class AnimalController {
 
