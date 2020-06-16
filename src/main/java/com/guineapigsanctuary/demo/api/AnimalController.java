030824c9ad28cb3f67d210da788b74d28d6b2a6d
roberthaworth1234 <roberthaworth1234@hotmail.com>
2020-06-16 12:16:47 +0100

    reamoving cors in controller

--- src/main/java/com/guineapigsanctuary/demo/api/AnimalController.java
+++ src/main/java/com/guineapigsanctuary/demo/api/AnimalController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("api/v1/animal")
-@CrossOrigin(origins = "https://guineapig-sanctuary.herokuapp.com/", maxAge = 3600)
 @RestController
 public class AnimalController {
 
