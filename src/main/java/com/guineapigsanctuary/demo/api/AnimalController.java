540ab9697199d01b1add5ed856dd929dde98860f
roberthaworth1234 <roberthaworth1234@hotmail.com>
2020-06-16 16:31:29 +0100

    adding cors to get by ID

--- src/main/java/com/guineapigsanctuary/demo/api/AnimalController.java
+++ src/main/java/com/guineapigsanctuary/demo/api/AnimalController.java
@@ -34,7 +34,7 @@ public class AnimalController {
        return animalService.getAllAnimals();
     }
 
-    @CrossOrigin("https://guineapig-sanctuary.herokuapp.com/api/v1/animal")
+    @CrossOrigin
     @GetMapping(path="/{id}")
     public Animal getAnimalbyId(@PathVariable("id") UUID id) {
        return  animalService.getAnimalById(id)
