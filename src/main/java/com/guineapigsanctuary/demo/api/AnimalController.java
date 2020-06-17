411e5802972dc075051f9bc657d27d740f13d5ee
roberthaworth1234 <roberthaworth1234@hotmail.com>
2020-06-17 10:03:42 +0100

    adding cross origin to the class

--- src/main/java/com/guineapigsanctuary/demo/api/AnimalController.java
+++ src/main/java/com/guineapigsanctuary/demo/api/AnimalController.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
-
+@CrossOrigin("https://guineapig-sanctuary.herokuapp.com/")
 @RequestMapping("api/v1/animal")
 @RestController
 public class AnimalController {
